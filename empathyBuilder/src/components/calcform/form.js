89016c15b0bd2fc2841c3efbe86e0c7f9211cbb0
lschimm <lschimmelfennig@gmail.com>
2019-06-25 14:56:57 -0500

    tring to make form calculations work

--- empathyBuilder/src/components/calcform/form.js
+++ empathyBuilder/src/components/calcform/form.js
@@ -1,12 +1,48 @@
 import React, { Component } from "react";
 import { Button } from "reactstrap";
+import Calculation from "./form2";
 import "./form.css";
 
 export default class Form extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      result: "",
+      transportation: "",
+      healthInsurance: "",
+      carInsurance: ""
+    };
   }
+
+  onClick = button => {
+    if (button === "button") {
+      this.calculate();
+    } else {
+      this.setState({
+        result: this.state.result + button
+      });
+    }
+  };
+
+  calculate = () => {
+    var checkResult = "";
+    if (this.state.result.includes("--")) {
+      checkResult = this.state.result.replace("--", "+");
+    } else {
+      checkResult = this.state.result;
+    }
+
+    try {
+      this.setState({
+        // eslint-disable-next-line
+        result: (eval(checkResult) || "") + ""
+      });
+    } catch (e) {
+      this.setState({
+        result: "error"
+      });
+    }
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -23,7 +59,7 @@ export default class Form extends React.Component {
               className="forms"
               onChange={this.handleInputChange}
               placeholder="Expense"
-              //value
+              // value={this.state.transporatation}
               name="price"
             />
             Health Insurance:{" "}
@@ -31,7 +67,7 @@ export default class Form extends React.Component {
               className="forms"
               onChange={this.handleInputChange}
               placeholder="Expense"
-              //value
+              // value={this.state.healthInsurance}
               name="price"
             />
             Car Insurance:{" "}
@@ -39,7 +75,7 @@ export default class Form extends React.Component {
               className="forms"
               onChange={this.handleInputChange}
               placeholder="Expense"
-              //value
+              value={this.state.carInsurance}
               name="price"
             />
             Main Loans:{" "}
@@ -67,9 +103,72 @@ export default class Form extends React.Component {
               name="price"
             />
           </form>
-          <Button type="submit" className="button" color="info">
-            Calculate
-          </Button>
+          <div className="totalGroup">
+            <h3>Calculate Monthly Relocation Cost</h3>
+            <form className="mainForm">
+              Total Hotel Cost/Home Rental Fee:{" "}
+              <input
+                className="forms"
+                onChange={this.handleInputChange}
+                placeholder="Expense"
+                //value
+                name="price"
+              />
+              Utility Conection Fees:{" "}
+              <input
+                className="forms"
+                onChange={this.handleInputChange}
+                placeholder="Expense"
+                //value
+                name="price"
+              />
+              Storage Unit:{" "}
+              <input
+                className="forms"
+                onChange={this.handleInputChange}
+                placeholder="Expense"
+                //value
+                name="price"
+              />
+              Car Rental/Moving Services:{" "}
+              <input
+                className="forms"
+                onChange={this.handleInputChange}
+                placeholder="Expense"
+                //value
+                name="price"
+              />
+              Gas:{" "}
+              <input
+                className="forms"
+                onChange={this.handleInputChange}
+                placeholder="Expense"
+                //value
+                name="price"
+              />
+              Additional Security Measures:{" "}
+              <input
+                className="forms"
+                onChange={this.handleInputChange}
+                placeholder="Expense"
+                //value
+                name="price"
+              />
+              Miscellaneous:{" "}
+              <input
+                className="forms"
+                onChange={this.handleInputChange}
+                placeholder="Expense"
+                //value
+                name="price"
+              />
+            </form>
+            <Button type="submit" className="button" color="info">
+              Calculate
+            </Button>
+            <button type="onClick" />
+            <Calculation result={this.state.result} />
+          </div>
         </div>
       </div>
     );
